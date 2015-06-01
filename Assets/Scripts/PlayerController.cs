12e6eaf484634a224b3941cc20a6a392d65aff6d
Matt <bfc.shbai@gmail.com>
2015-06-01 11:25:28 +1000

    Added the game
    Imported the game to git hub

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Input.GetKeyDown(KeyCode.UpArrow))
+		{
+
+		}
+		if(Input.GetKeyDown(KeyCode.DownArrow))
+		{
+			
+		}
+	}
+}
